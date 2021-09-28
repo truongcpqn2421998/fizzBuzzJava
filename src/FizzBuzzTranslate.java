0d0da80347a791c644f8d0491c61921e370e74dd
truongcpqn2421998 <truongcpqn24219982@gmail.com>
2021-09-28 16:39:39 +0700

    finish

--- src/FizzBuzzTranslate.java
+++ src/FizzBuzzTranslate.java
@@ -1,5 +1,70 @@
 public class FizzBuzzTranslate {
     public static String translate(int number){
-        return "Fizz";
+        String trans;
+        if(checkBuzz(number) && checkFizz(number)){
+            trans="FizzBuzz";
+        }else if(checkFizz(number)){
+            trans= "Fizz";
+        }else if(checkBuzz(number)){
+            trans="Buzz";
+        }else {
+            trans=checkNumber(number);
+        }
+        return trans;
     }
+    public static boolean checkFizz(int number){
+        boolean tens=(number-number%10)/10==3;
+        boolean unis=number%10==3;
+        if (number %3==0){
+            return true;
+        }
+        if (tens||unis){
+            return true;
+        }
+        return false;
+    }
+    public static boolean checkBuzz(int number){
+        int tens=(number-number%10)/10;
+        int unis=number%10;
+        if (number %5==0){
+            return true;
+        }
+        if (tens==5||unis==5){
+            return true;
+        }
+        return false;
+    }
+    public static String checkNumber(int number){
+        String trans;
+        if(number>10) {
+            int tens = (number - number % 10) / 10;
+            int unis = number % 10;
+            trans = read_a_digit(tens) +" "+ read_a_digit(unis);
+        }else {
+            trans=read_a_digit(number);
+        }
+        return trans;
+    }
+
+    private static String read_a_digit(int number) {
+        String trans=null;
+        switch (number){
+            case 1: trans="Một";
+                    break;
+            case 2: trans="Hai";
+                    break;
+            case 4: trans="Bốn";
+                    break;
+            case 6: trans="Sáu";
+                    break;
+            case 7: trans="Bảy";
+                    break;
+            case 8: trans="Tám";
+                    break;
+            case 9: trans="Chín";
+                    break;
+        }
+        return trans;
+    }
+
 }
--- test/FizzBuzzTranslateTest.java
+++ test/FizzBuzzTranslateTest.java
@@ -11,7 +11,7 @@ class FizzBuzzTranslateTest {
          String result=FizzBuzzTranslate.translate(numberTest);
          assertEquals(expected,result);
     }
-    @DisplayName("number :4")
+    @DisplayName("number :5")
     @org.junit.jupiter.api.Test
     void test2Translate() {
         int numberTest=5;
@@ -19,4 +19,37 @@ class FizzBuzzTranslateTest {
         String result=FizzBuzzTranslate.translate(numberTest);
         assertEquals(expected,result);
     }
+    @DisplayName("number :16")
+    @org.junit.jupiter.api.Test
+    void test3Translate() {
+        int numberTest=16;
+        String expected="Một Sáu";
+        String result=FizzBuzzTranslate.translate(numberTest);
+        assertEquals(expected,result);
+    }
+    @DisplayName("number :16")
+    @org.junit.jupiter.api.Test
+    void test4Translate() {
+        int numberTest=31;
+        String expected="Fizz";
+        String result=FizzBuzzTranslate.translate(numberTest);
+        assertEquals(expected,result);
+    }
+    @DisplayName("number :7")
+    @org.junit.jupiter.api.Test
+    void test5Translate() {
+        int numberTest=7;
+        String expected="Bảy";
+        String result=FizzBuzzTranslate.translate(numberTest);
+        assertEquals(expected,result);
+    }
+    @DisplayName("number :64")
+    @org.junit.jupiter.api.Test
+    void test6Translate() {
+        int numberTest=64;
+        String expected="Sáu Bốn";
+        String result=FizzBuzzTranslate.translate(numberTest);
+        assertEquals(expected,result);
+    }
+
 }
